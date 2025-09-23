52b7a9667cfb3db332797c0ceec09bfe0bd31206
Murat Duman <dumanmurat9@gmail.com>
2025-09-24 01:02:07 +0300

    test

--- infra/terraform/gcp/modules/platform/ci_cd/main.tf
+++ infra/terraform/gcp/modules/platform/ci_cd/main.tf
@@ -1,5 +1,3 @@
-# Platform ci_cd modulunde GitHub tetikleyicili Cloud Build kaynagi olusturulur.
-
 resource "google_cloudbuild_trigger" "github" {
   name     = "${var.name_prefix}-trigger"
   project  = var.project_id
@@ -8,9 +6,18 @@ resource "google_cloudbuild_trigger" "github" {
   description = "GitHub push ile otomatik build"
   filename    = var.build_filename
 
-  github {
-    owner = var.github_owner
-    name  = var.github_repo
+  # Eski github formatını kaldırın
+  # github {
+  #   owner = var.github_owner
+  #   name  = var.github_repo
+  #   push {
+  #     branch = var.github_branch
+  #   }
+  # }
+
+  # Yeni repository_event_config formatını kullanın
+  repository_event_config {
+    repository = "projects/${var.project_id}/locations/${var.location}/connections/qr-photo-connection/repositories/xofyy-qr-photo-app"
     push {
       branch = var.github_branch
     }
@@ -18,9 +25,9 @@ resource "google_cloudbuild_trigger" "github" {
 
   included_files = var.included_files
   ignored_files  = var.ignored_files
-
   substitutions = var.substitutions
 
+  service_account = "projects/${var.project_id}/serviceAccounts/305617730642-compute@developer.gserviceaccount.com"
 
   tags = ["terraform", "cloud-build", var.environment]
 }
